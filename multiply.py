2549cc39d84eeddb32272c7be6342b8c0e654efb
RanranYang1110 <muzi_swjtu@163.com>
2019-09-11 19:51:28 +0800

    输出结构由数字转成字符串

--- multiply.py
+++ multiply.py
@@ -8,10 +8,9 @@ def multiply(num1, num2):
     else:
         a, b = num2, num1
     sum1 = 0
-
     for i in range(len(b)):
         sum1 += (int(b[i]) * 10 ** (len(b) - i  - 1)) * int(a)
-    return sum1
+    return str(sum1)
 num1, num2 = '10', '20'
 print(multiply(num1, num2))
 print(int(num1) * int(num2))
