448470e9a91b66e7d588e5e2285cfc8679a3076b
RanranYang1110 <muzi_swjtu@163.com>
2019-09-11 19:37:58 +0800

    字符串相乘

---
+++ multiply.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+# @author: qianli
+# @file: multiply.py
+# @time: 2019/09/11
+def multiply(num1, num2):
+    if len(num1) > len(num2):
+        a, b = num1, num2
+    else:
+        a, b = num2, num1
+    sum1 = 0
+
+    for i in range(len(b)):
+        sum1 += (int(b[i]) * 10 ** (len(b) - i  - 1)) * int(a)
+    return sum1
+num1, num2 = '10', '20'
+print(multiply(num1, num2))
+print(int(num1) * int(num2))
